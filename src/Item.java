54ca17f75b94bd7638f47de40a03fe7add090805
Queen-23-9 <tqueen1393@gmail.com>
2026-03-31 12:04:26 +0700

    thêm Item

---
+++ src/Item.java
@@ -0,0 +1,40 @@
+public abstract class Item implements Entity {
+    protected String itemId;
+    protected String name;
+    protected String description;
+    protected double startingPrice;
+
+    public Item(String itemId, String name, String description, double startingPrice) {
+        this.itemId = itemId;
+        this.name = name;
+        this.description = description;
+        this.startingPrice = startingPrice;
+    }
+
+    @Override
+    public String getId() {
+        return itemId;
+    }
+
+    public String getItemId() {
+
+        return itemId;
+    }
+
+    public String getName() {
+
+        return name;
+    }
+
+    public String getDescription() {
+
+        return description;
+    }
+
+    public double getStartingPrice() {
+
+        return startingPrice;
+    }
+
+    public abstract void printInfo();
+}
