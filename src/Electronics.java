bcaece435cd50673e3c941c5acdb8a98c582ce0f
Quyên Tống <25021966@vnu.edu.vn>
2026-04-05 21:31:38 +0700

    thêm Electronics

---
+++ src/Electronics.java
@@ -0,0 +1,18 @@
+public class Electronics extends Item {
+
+    private String brand;
+    private int warrantyMonths;
+
+    public Electronics(String itemId, String name, String description,
+                       double startingPrice, String brand, int warrantyMonths) {
+
+        super(itemId, name, description, startingPrice);
+        this.brand = brand;
+        this.warrantyMonths = warrantyMonths;
+    }
+
+    @Override
+    public void printInfo() {
+
+    }
+}
