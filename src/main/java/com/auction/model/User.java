90c44620f4fa1e08d29522992f3c7af2c70164b8
PHU <vupu2007@gmail.com>
2026-05-05 23:01:18 +0700

    Update User

--- src/main/java/com/auction/model/User.java
+++ src/main/java/com/auction/model/User.java
@@ -1,42 +1,34 @@
 package com.auction.model;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
-
 public abstract class User implements Entity {
     protected String id;
     protected String username;
     protected String password;
+    protected String email; // Thêm mới
+    protected String role;  // Thêm mới
 
-    public User(String id, String username,String password){
+    public User(String id, String username, String password, String email, String role) {
         this.id = id;
         this.username = username;
         this.password = password;
-    }
-    public String getUsername(){
-        return username;
-    }
-    public void setUsername(String username){
-        this.username = username;
-    }
-    public String getPassword(){
-        return password;
-    }
-    public void setPassword(String password){
-        this.password = this.password;
-    }
-    public String getId(){
-        return id;
+        this.email = email;
+        this.role = role;
     }
 
-//    public boolean login(String inputPassword){
-//        return this.password.equals(inputPassword);
-//
-//    }
-//    public boolean register(){
-//        //lưu user mới vào database thông qua lớp DAO(Data Access Object)
-//        System.out.println("Đăng kí tài khoản thành công cho user: "+ this.username);
-//           return true;
-//    }
+    // Getter và Setter cho các trường mới
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+
+    public String getRole() { return role; }
+    public void setRole(String role) { this.role = role; }
+
+    public String getUsername() { return username; }
+    public void setUsername(String username) { this.username = username; }
+
+    public String getPassword() { return password; }
+    public void setPassword(String password) { this.password = password; }
+
+    public String getId() { return id; }
+
     public abstract String displayRole();
 }
