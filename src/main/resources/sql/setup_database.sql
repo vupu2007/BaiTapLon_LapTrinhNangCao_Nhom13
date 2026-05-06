e81bd3adbdba2ab80f306b1da125561016555690
PHU <vupu2007@gmail.com>
2026-05-06 15:31:35 +0700

    Thêm số dư vào database

--- src/main/resources/sql/setup_database.sql
+++ src/main/resources/sql/setup_database.sql
@@ -7,7 +7,8 @@ CREATE TABLE Users (
                        user_id INT AUTO_INCREMENT PRIMARY KEY,
                        username VARCHAR(50) UNIQUE NOT NULL,
                        password VARCHAR(255) NOT NULL,
-                       role ENUM('ADMIN', 'SELLER', 'BIDDER') DEFAULT 'BIDDER'
+                       role ENUM('ADMIN', 'SELLER', 'BIDDER') DEFAULT 'BIDDER' ,
+                       balance DOUBLE DEFAULT 0.0
 );
 
 -- 3. Bảng Categories
