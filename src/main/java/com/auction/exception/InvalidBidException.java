6d39eb106fa66fd969c7b05a8986d4a24a6d618c
Quyên Tống <25021966@vnu.edu.vn>
2026-05-10 20:26:39 +0700

    thêm

---
+++ src/main/java/com/auction/exception/InvalidBidException.java
@@ -0,0 +1,28 @@
+package com.auction.exception;
+
+/**
+ * Ném ra khi người dùng đặt giá không hợp lệ:
+ * - Giá đặt thấp hơn hoặc bằng giá hiện tại
+ * - Giá đặt không đủ bước tăng tối thiểu (minIncrement)
+ * - Giá đặt âm hoặc bằng 0
+ */
+public class InvalidBidException extends Exception {
+
+    private final double attemptedAmount;
+    private final double minimumRequired;
+
+    public InvalidBidException(String message) {
+        super(message);
+        this.attemptedAmount = 0;
+        this.minimumRequired = 0;
+    }
+
+    public InvalidBidException(String message, double attemptedAmount, double minimumRequired) {
+        super(message);
+        this.attemptedAmount = attemptedAmount;
+        this.minimumRequired = minimumRequired;
+    }
+
+    public double getAttemptedAmount() { return attemptedAmount; }
+    public double getMinimumRequired() { return minimumRequired; }
+}
