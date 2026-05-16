ea0a5d80905443f74511c50889e79d5db39350b9
PHU <vupu2007@gmail.com>
2026-05-16 22:15:26 +0700

    Thêm Art

---
+++ src/main/java/com/auction/model/Art.java
@@ -0,0 +1,32 @@
+package com.auction.model;
+
+public class Art extends Item {
+    private String artist;  // Tên nghệ sĩ
+    private int year;       // Năm sáng tác
+
+    // 1. Constructor rỗng
+    public Art() {
+        super();
+    }
+
+    // 2. Constructor đầy đủ
+    public Art(String itemId, String name, String description,
+               double startingPrice, int ownerId, int categoryId, String status,
+               String artist, int year) {
+        super(itemId, name, description, startingPrice, ownerId, categoryId, status);
+        this.artist = artist;
+        this.year = year;
+    }
+
+    public String getArtist() { return artist; }
+    public void setArtist(String artist) { this.artist = artist; }
+
+    public int getYear() { return year; }
+    public void setYear(int year) { this.year = year; }
+
+    @Override
+    public void printInfo() {
+        System.out.println("ID: " + itemId + " | Tên: " + name + " | Giá khởi điểm: " + startingPrice);
+        System.out.println("Nghệ sĩ: " + artist + " | Năm sáng tác: " + year);
+    }
+}
