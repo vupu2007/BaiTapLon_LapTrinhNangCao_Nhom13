150de588773d78e8a22f5d72e5fc307bcedf16fe
PHU <vupu2007@gmail.com>
2026-05-19 23:23:20 +0700

    Database về local

--- src/main/java/com/auction/server/util/DatabaseConnection.java
+++ src/main/java/com/auction/server/util/DatabaseConnection.java
@@ -6,9 +6,9 @@ import java.sql.SQLException;
 
 public class DatabaseConnection {
 
-    private static final String URL      = "jdbc:mariadb://localhost:3306";
+    private static final String URL      = "jdbc:mariadb://localhost:3306/online_auction_db";
     private static final String USER     = "root";
-    private static final String PASSWORD = "UdU6CGM3MX";
+    private static final String PASSWORD = "";
 
     // Singleton — chỉ 1 instance của class này tồn tại
     private static DatabaseConnection instance;
