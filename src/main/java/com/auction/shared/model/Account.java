60c54aa4bbf6d265795227fe1e65369bfa9ab8f8
nguyenthituyetnhung <25021926@vnu.edu.vn>
2026-06-07 14:40:26 +0700

    refactor code smell

--- src/main/java/com/auction/shared/model/Account.java
+++ src/main/java/com/auction/shared/model/Account.java
@@ -11,7 +11,6 @@ public abstract class Account implements Entity,Serializable {
     protected String password;
     protected String role;
     protected String email;
-    private boolean isLocked;
     // BỔ SUNG: Thuộc tính số dư ví tiền dùng chung cho các tài khoản kế thừa
     protected Double balance;
 
@@ -50,14 +49,6 @@ public abstract class Account implements Entity,Serializable {
     }
     public void setId(String id) {
         this.id = id;
-    }
-
-    public boolean isLocked() {
-        return isLocked;
-    }
-
-    public void setLocked(boolean isLocked) {
-        this.isLocked = isLocked;
     }
 
     // --- PHƯƠNG THỨC TRỪU TƯỢNG ---
